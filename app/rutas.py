bef852828dd5512e8a4f739ff4fa8ce6ab48cc9a
Jiayich0 <jiayich.p@gmail.com>
2025-04-19 17:45:57 +0200

    Finished: mostrar ediciones/paises

--- app/rutas.py
+++ app/rutas.py
@@ -25,11 +25,18 @@ def mostrar_ediciones():
     elementos_por_pagina = 5
 
     # AQUI VA VUESTRO CODIGO
-    total_elementos = mongo.db.festivales.count_documents({})
+    # Nota importante:
+    #   en find({}, {})
+    #       el primer {} es el filtro, WHERE en SQL
+    #       el segundo {} son las columnas/campos que quieres o no traer, SELECT en SQL
+    #           {"x":1, "y":1} : obtienes los campos x e y (+ campo id, por defecto)
+    #           {"x":0, "y":0} : obtienes todos los campos excepto x e y
+    #           {"x":1, "y":0} : error, no se pueden mezclar 1 y 0 (excepto id que puede ser 0)
+    total_elementos = mongo.db["festivales"].count_documents({})
     skip = (pagina - 1) * elementos_por_pagina # Paginacion: empeiza en el 0, 5, 10, etc
     festivales = list(
         mongo.db["festivales"]
-        .find({}, {"anyo": 1, "ciudad": 1, "pais": 1, "fecha": 1, "concursantes": 1}) # trae solo los elemntos/columnas seleccionadas
+        .find({}, {"anyo":1, "ciudad":1, "pais":1, "fecha":1, "concursantes":1})
         .sort("anyo", -1)               # Primero los ordena y luego hace el skip y limite por pagina
         .skip(skip)                     # Empiezan desde x pagina, cada 5 (paginate)
         .limit(elementos_por_pagina)    # 5 por pagina (paginate)
@@ -60,7 +67,7 @@ def mostrar_festival(anyo: int):
         participaciones=festival["concursantes"]
     )
 
-
+# TODO
 @app.route('/jugar')
 def jugar_quiz():
     # Jugar a un quiz. Esta funcion NO la teneis que modificar
@@ -86,7 +93,7 @@ def jugar_quiz():
 
     return render_template("juego.html", preguntas=preguntas, guardable=nombre is not None)
 
-
+# TODO
 @app.route('/quiz', methods=['GET', 'POST'])
 def generar_quiz():
     # Generacion de un quiz personalizado.
@@ -114,15 +121,53 @@ def mostrar_actuaciones_pais(id_pais: str):
     elementos_por_pagina = 10
 
     # AQUI VA VUESTRO CODIGO
+    if not id_pais:     # el problema es que solo verifca que id no sea ni null ni vacio pero si un pais no existe? -> if not participaciones
+        abort(404)
+
+    participaciones = []
+    nombre_pais = None
+    festivales = mongo.db["festivales"].find({}, {"anyo":1, "ciudad":1, "pais":1, "concursantes":1})
+    # o poner "fecha":0 pero no se si es del tod0 correcto
+
+    for festival in festivales:
+        anyo = festival["anyo"]
+        pais_host = festival["pais"]
+        ciudad = festival["ciudad"]
+
+        for concursante in festival["concursantes"]:
+            if concursante["id_pais"] == id_pais:
+                if nombre_pais is None:     # por pura eficiencia, si ya no es None deja de asignarle el nombre
+                    nombre_pais = concursante["pais"]
+
+                participaciones.append({
+                    "anyo": anyo,
+                    "pais_organizador": pais_host,
+                    "ciudad": ciudad,
+
+                    "artista": concursante["artista"],
+                    "cancion": concursante["cancion"],
+                    "resultado": concursante["resultado"],
+                    "puntuacion": concursante["puntuacion"],
+                    "url_youtube": concursante["url_youtube"]
+                })
+
+    if not participaciones: # pais dado no ha particiapdo en ningun año, "pais no existe"
+        abort(404)
+
+    total_elementos = len(participaciones)
+
+    skip = (pagina - 1) * elementos_por_pagina
+    inicio = skip
+    fin = skip + elementos_por_pagina
+    participaciones = participaciones[inicio:fin]
 
     # Descomentad cuando cargueis la informacion
-    # paginacion = render_pagination(pagina, elementos_por_pagina, total_elementos, 'mostrar_actuaciones_pais', id_pais=id_pais)
+    paginacion = render_pagination(pagina, elementos_por_pagina, total_elementos, 'mostrar_actuaciones_pais', id_pais=id_pais)
 
-    # return render_template("mostrar_actuaciones_pais.html", pais=nombre_pais, participaciones=participaciones,
-    #                        pagination=paginacion, pagina=pagina)
-    abort(404)
+    return render_template("mostrar_actuaciones_pais.html", pais=nombre_pais, participaciones=participaciones,
+                           pagination=paginacion, pagina=pagina)
 
-
+# TODO
 @app.route("/upload_contest", methods=["POST"])
 def guardar_concurso():
     # Guarda un concurso personalizado.
@@ -147,7 +192,7 @@ def guardar_concurso():
     # bien. En su lugar, devolvemos la respuesta en un json
     return {'redirect': url_for('mostrar_quizzes')}
 
-
+# TODO
 @app.route("/quizzes")
 def mostrar_quizzes():
     # Muestra la lista de quizzes en la aplicacion, utilizando una paginacion.
@@ -169,7 +214,7 @@ def mostrar_quizzes():
     #                        pagination=paginacion, pagina=pagina)
     abort(404)
 
-
+# TODO
 @app.route("/jugar/<nombre_quiz>")
 def jugar_quiz_personalizado(nombre_quiz: str):
     # Juega a un quiz que ha sido creado previamente por un usuario
