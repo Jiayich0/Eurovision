fecca3fdd7276d55bf67bb41d83d9929c60230eb
Jiayich0 <jiayich.p@gmail.com>
2025-04-23 20:53:12 +0200

    Changed: NombreCancion

--- app/trivia/videos.py
+++ app/trivia/videos.py
@@ -2,7 +2,7 @@
 Modulo para hacer cuestiones de trivia relacionadas con videos de Youtube. Creamos una clase TriviaVideo que extiende
 a Trivia y almacena el id de reproduccion de video
 """
-
+import random
 from abc import ABC, abstractmethod
 from typing import List
 from pathlib import Path
@@ -88,9 +88,38 @@ class NombreCancion(TriviaVideo):
     NOTA: para dificultar la respuesta, se deben seleccionar canciones del mismo pais.
     """
     def __init__(self, parametros: OperacionesEurovision):
-        self._url = None
-        self._opciones_invalidas = None
-        self._respuesta = None
+        participacion = parametros.participacion_aleatoria(1)[0]
+
+        """ URL """
+        self._url = participacion["url_youtube"]
+
+        """ RESPUESTA CORRECTA """
+        self._respuesta = participacion["cancion"]
+
+        """ RESPUESTAS INCORRECTAS """
+        pipeline = [
+            {"$unwind": "$concursantes"},
+            {"$match": {
+                "concursantes.pais": participacion["pais"],
+                "concursantes.cancion":{"$ne": self._respuesta}}
+            },
+            {"$sample": {"size": 3}},
+            {"$project": {"concursantes.cancion":1, "_id":0}}
+        ]
+        canciones_agregacion = list(parametros.agregacion(pipeline))
+        canciones = [c["concursantes"]["cancion"] for c in canciones_agregacion]
+
+        self._opciones_invalidas = canciones
+        # FIXME preguntar
+        """
+        He pensado en varias opciones para las respuestas invalidas de cara a la eficiencia:
+            1. Agregación, O(n), devolviendo una lista de dict. Transformo 'list de dict' en 'list de string', O(n).
+               Random.sample de O(k) (k=3). Conversion a String O(k)
+            2. Agregacion con {"$sample": {"size": 3}}, devolviendo una lista de dict de solo 3, O(k). Conversion a String O(k)
+               Sample documentacion: https://www.mongodb.com/docs/manual/reference/operator/aggregation/sample/ 
+               (no sale en la teoria, no se si puedo usarlo, preguntar al profe)
+            3. Incluso se podria hacer con un sort + rand + limit:3 (no se que eficiencia tendría)
+        """
 
     @property
     def url(self) -> str:
@@ -120,6 +149,11 @@ class InterpreteCancion(TriviaVideo):
     NOTA: para dificultar la respuesta, se deben seleccionar interpretes del mismo pais.
     """
     def __init__(self, parametros: OperacionesEurovision):
+        """ URL """
+
+        """ RESPUESTA CORRECTA """
+
+        """ RESPUESTAS INCORRECTAS """
         self._url = None
         self._opciones_invalidas = None
         self._respuesta = None
